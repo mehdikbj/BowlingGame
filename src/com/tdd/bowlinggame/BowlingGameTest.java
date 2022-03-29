5a43b48b1941c1950950dccf2319e11775f4e2ac
mkabbaj <mehdikabb@gmail.com>
2022-03-29 15:22:50 +0200

    Second test failed

--- src/com/tdd/bowlinggame/BowlingGameTest.java
+++ src/com/tdd/bowlinggame/BowlingGameTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// Game creation is duplicated
+// roll loop is duplicated
 class BowlingGameTest {
     @Test
     public void testGutterGame() throws Exception {
@@ -12,4 +14,13 @@ class BowlingGameTest {
             game.roll(0);
         assertEquals(0, game.score());
     }
+
+    @Test
+    public void testAllOnes() throws Exception {
+        BowlingGame game = new BowlingGame();
+        for (int i = 0; i < 20; i++)
+            game.roll(1);
+        assertEquals(20, game.score());
+    }
+
 }
