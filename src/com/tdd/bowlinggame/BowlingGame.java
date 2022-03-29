be8b9b8bd4f662ddbe5943832416272b7b994495
mkabbaj <mehdikabb@gmail.com>
2022-03-29 14:49:23 +0200

    init Bowling Game KATA- TDD

---
+++ src/com/tdd/bowlinggame/BowlingGame.java
@@ -0,0 +1,4 @@
+package com.tdd.bowlinggame;
+
+public class BowlingGame {
+}
---
+++ src/com/tdd/bowlinggame/BowlingGameTest.java
@@ -0,0 +1,12 @@
+package com.tdd.bowlinggame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BowlingGameTest {
+    @Test
+    public void testGutterGame() throws Exception {
+        BowlingGame g = new BowlingGame();
+    }
+}
